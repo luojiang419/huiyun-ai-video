6323bc8c71937ed85039da3b0cbe5243c37d08d9
luojiang419 <130038300+luojiang419@users.noreply.github.com>
2026-07-07 17:10:50 +0800

    发布 V9.9 测试版本

--- flutter_grsai_video_gen/lib/constants/app_version.dart
+++ flutter_grsai_video_gen/lib/constants/app_version.dart
@@ -1,3 +1,3 @@
-const String appReleaseVersion = 'V9.8';
+const String appReleaseVersion = 'V9.9';
 const String appDisplayTitle = '绘云AI $appReleaseVersion';
 const String appVersionLabel = '版本 $appReleaseVersion';
